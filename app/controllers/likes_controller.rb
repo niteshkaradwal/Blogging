0c4545e9c6480f7ac2f0d116180ed6ceba37b712
rails <rails@rails.(none)>
2013-02-11 13:30:17 +0530

    like for post

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,8 @@
 class LikesController < ApplicationController
+	
+	def new
+		@post = Post.find(params[:post_id])
+		@post.likes.create(:user_id => current_user.id)
+		redirect_to post_path(@post)
+	end	
 end
