a7ba431353c0e756b38f585c2136862bb607169d
nitesh <nitesh@grepruby.com>
2013-02-11 20:26:50 +0530

    Hide unnecessary data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130209105327) do
+ActiveRecord::Schema.define(:version => 20130211134041) do
 
   create_table "comments", :force => true do |t|
     t.string   "content"
@@ -61,8 +61,9 @@ ActiveRecord::Schema.define(:version => 20130209105327) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.string   "gender",                 :default => "", :null => false
+    t.string   "gender",                                 :null => false
     t.date     "birth_date",                             :null => false
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
