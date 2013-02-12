9bc6bccfef2e6a9521d223c86024458ae0ff0b24
nitesh <nitesh@grepruby.com>
2013-02-12 17:31:00 +0530

    like and unlike for post

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,4 +5,12 @@ class LikesController < ApplicationController
 		@post.likes.create(:user_id => current_user.id)
 		redirect_to post_path(@post)
 	end	
+	
+	def destroy
+	@post = Post.find(params[:post_id])
+	@like = Like.find(params[:id])
+	@like.destroy
+	redirect_to post_path(@post)
+	end
+	
 end
