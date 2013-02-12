6dde8d482807376efc3b66ad7b7d5854a9ac4178
nitesh <nitesh@grepruby.com>
2013-02-12 17:31:36 +0530

    Like for comments

--- app/controllers/likecomments_controller.rb
+++ app/controllers/likecomments_controller.rb
@@ -5,4 +5,12 @@ class LikecommentsController < ApplicationController
 		@post = Post.find(params[:post_id])
 		redirect_to post_path(@post)
 	end	
+	
+	def destroy
+		@post = Post.find(params[:post_id])
+		@like = Like.find(params[:id])
+		@like.destroy
+		redirect_to post_path(@post)
+	end
+	
 end
