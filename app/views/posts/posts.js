24c985aaa16aaf6923934ece588c23effc997f43
nitesh <nitesh@grepruby.com>
2013-02-21 17:18:55 +0530

    use helper in views

---
+++ app/views/posts/posts.js
@@ -0,0 +1,13 @@
+
+
+
+<% content_for :head do %>
+  <script type="text/javascript">
+    $(document).ready(function(){
+  
+    $("#tab").hide();
+    $("#tab").show(1000);
+  
+});
+  </script>
+<% end %>
