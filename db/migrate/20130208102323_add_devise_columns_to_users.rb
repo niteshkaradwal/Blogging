58bb41877ca0a44fd89658781a4719307cc4011c
nitesh <nitesh@grepruby.com>
2013-02-21 12:29:13 +0530

    remove deleted file

--- db/migrate/20130208102323_add_devise_columns_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddDeviseColumnsToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :gender, :string, :null => false
-    add_column :users, :birth_date, :date, :null => false
-  end
-end
