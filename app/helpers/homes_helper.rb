0de6d8467c41b759e3334c7086a8cb826a3a3667
nitesh <nitesh@grepruby.com>
2013-02-23 11:53:28 +0530

    use helpers for show posts

--- app/helpers/homes_helper.rb
+++ app/helpers/homes_helper.rb
@@ -1,14 +1,19 @@
 module HomesHelper
 
-  def show_post_of_followers(user)
-    user = User.find(user)
-    user.posts.each do |post|
-      link = link_to " #{ user.name } ", post, :class => "btn btn-mini btn-primary"  
-      email = user.email 
-      title = post.title
-      post = post.body 
-    return link,email,title,post
-    end
+  def show_post_of_followers(follower,post)
+    
+    
+
+     
+
+        link = link_to " #{ follower.name } ", post, :class => "btn btn-mini btn-primary"  
+        email = follower.email 
+        title = post.title
+        post = post.body 
+      return link,email,title,post
+      
+      
+   
     
   end  
       
--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,2 +1,10 @@
 module LikesHelper
+
+  def search_for_like(post)
+    return post.likes.where(:user_id => current_user.id)[0]
+  end
+  
+  def like_exist_or_not?(post)
+    post.likes.where(:user_id => current_user.id)[0].blank?
+  end
 end
